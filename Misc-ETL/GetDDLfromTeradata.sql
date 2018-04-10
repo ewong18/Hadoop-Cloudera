20f66da00e900fd51e95d58d62413cf11387624b
ewong18 <37660460+ewong18@users.noreply.github.com>
2018-04-10 14:54:11 -0400

    Update GetDDLfromTeradata.sql

--- Misc-ETL/GetDDLfromTeradata.sql
+++ Misc-ETL/GetDDLfromTeradata.sql
@@ -6,36 +6,42 @@
 --
 
  select c2.columnid, c1.tablename, 
-					trim(c1.columnname)||' '||case when c1.columntype = 'CV' then 'string'||','
+					case when c1.columntype in ('CV') then 'cast(oreplace(oreplace(oreplace('||trim(c1.columnname)
+										||', chr(10), '' ''), ''^'', '' ''), chr(13), '' '') as varchar'
+										||trim(substr(c1.columnformat, 2,20))||') as '||trim(c1.columnname)
+							when c1.columntype in ('CF') then 'cast(oreplace(oreplace(oreplace('||trim(c1.columnname)
+										||', chr(10), '' ''), ''^'', '' ''), chr(13), '' '') as char'
+										||trim(substr(c1.columnformat, 2,20))||') as '||trim(c1.columnname)
+							else c1.columnname
+							end ||',' as td_oreplace,
+ 					trim(c1.columnname)||' '||case when c1.columntype = 'CV' then 'string'||','
 							when c1.columntype = 'CF' then 'string'||','
 							when c1.columntype = 'I' then 'int'||','
-							when c1.columntype = 'D' then 'decimal'||'('||cast(c1.decimaltotaldigits as varchar(6))
-										||','||cast(c1.decimalfractionaldigits as varchar(6))||')'||','
+							when c1.columntype = 'D' then 'decimal('||cast(c1.decimaltotaldigits as varchar(6))
+														||','||cast(c1.decimalfractionaldigits as varchar(6))||')'||','
 							when c1.columntype in ('DA', 'TS') then 'string'||', -- Originally Date/TS in TD'					
 							else 'TBD' end  as hive_format,
 					case when c1.columntype in ('DA', 'TS') then 'cast('||trim(c1.columnname)||' as timestamp)'
 							else trim(c1.columnname)
-							end || ',' as impala_format,
+							end || ',' as imp_hiveSel_format,
+					trim(c1.columnname)||' '||case when c1.columntype = 'CV' then 'string'
+							when c1.columntype = 'CF' then 'string'
+							when c1.columntype = 'I' then 'int'
+							when c1.columntype = 'D' then 'decimal'||'('||cast(c1.decimaltotaldigits as varchar(6))
+														||','||cast(c1.decimalfractionaldigits as varchar(6))||')'
+							when c1.columntype in ('DA', 'TS') then 'timestamp'					
+							else 'TBD' end ||','  as impala_format,
  					trim(c1.columnname)||' '||case when c1.columntype = 'CV' then 'varchar'||trim(substr(c1.columnformat, 2,20))
 							when c1.columntype = 'CF' then 'char'||trim(substr(c1.columnformat, 2,20))
 							when c1.columntype = 'I' then 'Integer'
-							when c1.columntype = 'D' then 'decimal'||'('||cast(c1.decimaltotaldigits as varchar(6))
-										||','||cast(c1.decimalfractionaldigits as varchar(6))||')'
+							when c1.columntype = 'D' then 'decimal('||cast(c1.decimaltotaldigits as varchar(6))
+														||','||cast(c1.decimalfractionaldigits as varchar(6))||')'
 							when c1.columntype in ('DA', 'TS') then 'varchar(35)'							
-							else 'TBD' end ||',' as td_format,
-					case when c1.columntype in ('CV') then 'cast(oreplace(oreplace(oreplace('||trim(c1.columnname)
-										||', chr(10), ''' ' ' '''), ''' '^' ''', ''' ' ' '''), chr(13), ''' ' ' ''') as varchar'
-										||trim(substr(c1.columnformat, 2,20))||') as '||trim(c1.columnname)
-							when c1.columntype in ('CF') then 'cast(oreplace(oreplace(oreplace('||trim(c1.columnname)
-										||', chr(10), ''' ' ' '''), ''' '^' ''', ''' ' ' '''), chr(13), ''' ' ' ''') as char'
-										||trim(substr(c1.columnformat, 2,20))||') as '||trim(c1.columnname)
-							else c1.columnname
-							end ||',' as td_oreplace
-
+							else 'TBD' end ||',' as td_format
 		from dbc.columns c1
 		join dbc.columnsv c2
 			on  c1.tablename = c2.tablename
 			and c1.columnname = c2.columnname
-		where c1.databasename = 'TABLENAME'
-		and c2.databasename = 'VIEWNAME'
+		where c1.databasename = 'NACRMFBICOREDB'
+		and c2.databasename = 'NACRMFBISEMVW'
 	order by 2, 1 asc;
